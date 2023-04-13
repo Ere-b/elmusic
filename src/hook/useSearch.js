4b563138f399d66d251ff1a7113f25c64af2f82b
hanul <hanul.developer@gmail.com>
2023-04-13 11:57:20 +0900

    feat useSearch hook

---
+++ src/hook/useSearch.js
@@ -0,0 +1,42 @@
+import { useCallback, useState, useEffect } from 'react';
+import { fetchSearchAlbum } from '../services/service';
+import debounce from '../utils/debounce';
+
+const useSearch = () => {
+  const [searchText, setSearchText] = useState('');
+  const [searchResult, setSearchResult] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  const fetchData = async (searchText) => {
+    try {
+      setIsLoading(true);
+      const response = await fetchSearchAlbum(searchText);
+      console.log(response);
+      setSearchResult(response);
+      setIsLoading(false);
+    } catch (error) {
+      setIsError(true);
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (searchText.length > 0) {
+      fetchData(searchText);
+    } else {
+      setSearchResult(null);
+    }
+  }, [searchText]);
+
+  const handleSearch = useCallback(
+    debounce((searchText) => {
+      setSearchText(searchText);
+    }, 500),
+    []
+  );
+
+  return { searchResult, isLoading, isError, handleSearch };
+};
+
+export default useSearch;
