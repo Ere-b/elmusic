215459c331ed9f9c87126b06c83376e1cd1d1916
hanul <hanul.developer@gmail.com>
2023-04-13 09:09:55 +0900

    feat Top 100 albums api

--- src/services/service.js
+++ src/services/service.js
@@ -3,9 +3,28 @@ import axios from 'axios';
 const BASE_URL = 'https://itunes.apple.com';
 
 // Top 100 앨범 데이터 가져오기
-export const fetchTopAlbums = async () => {
-  const response = await axios.get(
-    `${BASE_URL}/us/rss/topalbums/limit=100/json`
-  );
-  return response.data.feed.entry;
+export const fetchTop100Albums = async () => {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}/us/rss/topalbums/limit=100/json`
+    );
+    return response.data.feed.entry;
+  } catch (error) {
+    alert('Failed to load data');
+  }
+};
+
+// 앨범 상세 데이터 가져오기
+export const fetchAlbum = async (id) => {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}/us/rss/topalbums/limit=100/json`
+    );
+    const album = response.data.feed.entry.find(
+      (entry) => entry.id.attributes['im:id'] === id
+    );
+    return album;
+  } catch (error) {
+    alert('Failed to load data');
+  }
 };
